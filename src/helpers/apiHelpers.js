eee7227c114d45ea366b99a148978996b6a074f9
elclubber <nykenn1@gmail.com>
2024-10-18 06:52:52 +0400

    refactor redux sagas to be more clear

---
+++ src/helpers/apiHelpers.js
@@ -0,0 +1,35 @@
+import APIConstants from '../constants/APIConstants';
+
+// Fetch all employees
+export const fetchEmployeesAPI = async () => {
+    const response = await fetch(APIConstants.EMPLOYEE_LIST);
+    if (!response.ok) {
+        throw new Error('Failed to fetch employees');
+    }
+    return response.json();
+};
+
+// Add a new employee
+export const addEmployeeAPI = async (employee) => {
+    const response = await fetch(APIConstants.ADD_EMPLOYEE, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(employee),
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to add employee');
+    }
+    return response.json();
+};
+
+// Delete an employee by ID
+export const deleteEmployeeAPI = async (id) => {
+    const response = await fetch(`${APIConstants.DELETE_EMPLOYEE}/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to delete employee');
+    }
+};
